f6853d0f33a6f125168baa66ab30afff3df33d0f
Real1w <realshimoney@gmail.com>
2025-08-25 20:15:26 -0400

    Update get-keys.js

--- api/get-keys.js
+++ api/get-keys.js
@@ -1,18 +1,5 @@
-const { getKeys } = require('./lib/memoryDB');
+import { getAllKeys } from "./_db";
 
-module.exports = function (req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  if (req.method === 'OPTIONS') return res.status(200).end();
-  if (req.method !== 'GET') return res.status(405).json({ success: false, error: 'Method not allowed' });
-
-  try {
-    const keys = getKeys();
-    return res.status(200).json({ success: true, keys });
-  } catch (err) {
-    console.error('get-keys error', err);
-    return res.status(500).json({ success: false, error: 'Internal server error' });
-  }
-};
+export default function handler(req, res) {
+  return res.json(getAllKeys());
+}
